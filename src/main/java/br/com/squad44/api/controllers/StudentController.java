cba9646a62ed78e4cc0581fa1b628453ad38aaa3
unknown <marcelo96comercial@gmail.com>
2021-03-25 16:31:30 -0300

    Adicionado metodo register

--- src/main/java/br/com/squad44/api/controllers/StudentController.java
+++ src/main/java/br/com/squad44/api/controllers/StudentController.java
@@ -1,9 +1,13 @@
 package br.com.squad44.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.squad44.api.entities.Student;
 import br.com.squad44.api.repositories.StudentRepository;
 
 @RestController
@@ -13,5 +17,9 @@ public class StudentController {
     @Autowired
     StudentRepository studentRepository;
 
-    
+    @PostMapping
+    public ResponseEntity<Student> register(@RequestBody Student form) {
+        Student student = new Student(form.getName(), form.getSchool(), form.getParent());
+        return ResponseEntity.ok().body(student);
+    }
 }
