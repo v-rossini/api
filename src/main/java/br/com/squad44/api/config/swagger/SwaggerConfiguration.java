88536abea6839ed9d9c625e5fe71b39fb17f830b
marcelomsilva <marcelo96comercial@gmail.com>
2021-03-26 10:17:45 -0300

    Adicionado config do Swagger e alterado versao

--- src/main/java/br/com/squad44/api/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/squad44/api/config/swagger/SwaggerConfiguration.java
@@ -1,5 +1,24 @@
 package br.com.squad44.api.config.swagger;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
 public class SwaggerConfiguration {
     
+    @Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.squad44.api"))
+				.paths(PathSelectors.ant("/**"))
+				.build();				
+	}
 }
