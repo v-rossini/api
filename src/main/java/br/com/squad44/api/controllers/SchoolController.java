7e83bea20d268052378cbb7c5fa4c2d12f4d90f1
unknown <marcelo96comercial@gmail.com>
2021-03-27 14:44:53 -0300

    Adicionado metodo getList no SchoolController

--- src/main/java/br/com/squad44/api/controllers/SchoolController.java
+++ src/main/java/br/com/squad44/api/controllers/SchoolController.java
@@ -1,9 +1,13 @@
 package br.com.squad44.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.squad44.api.entities.School;
 import br.com.squad44.api.repositories.SchoolRepository;
 
 @RestController
@@ -12,4 +16,9 @@ public class SchoolController {
     
     @Autowired
     SchoolRepository schoolRepository;
+
+    @GetMapping
+    public List<School> getList() {
+        return (List<School>) schoolRepository.findAll();
+    }
 }
