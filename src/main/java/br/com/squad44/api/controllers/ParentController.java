e1e86b93fe41530bee35cf67766635741c0e6fbe
unknown <marcelo96comercial@gmail.com>
2021-03-27 16:07:09 -0300

    Adicionado metodo register no ParentControler

--- src/main/java/br/com/squad44/api/controllers/ParentController.java
+++ src/main/java/br/com/squad44/api/controllers/ParentController.java
@@ -1,9 +1,13 @@
 package br.com.squad44.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.squad44.api.entities.Parent;
 import br.com.squad44.api.repositories.ParentRepository;
 
 @RestController
@@ -12,5 +16,12 @@ public class ParentController {
     
     @Autowired
     ParentRepository parentRepository;
-    
+
+
+    @PostMapping
+    public ResponseEntity<Parent> register(@RequestBody Parent form) {
+        Parent parent = new Parent(form.getName(), form.getPhone(), form.getCity(), form.getAddress(), form.getState(), form.getCpf());
+        parentRepository.save(parent);
+        return ResponseEntity.ok().build();
+    }
 }
