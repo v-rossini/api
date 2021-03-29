b41124cbb85af7907937f0dd62bc0a39d8f5be56
unknown <marcelo96comercial@gmail.com>
2021-03-29 18:05:57 -0300

    Adicionado estrutura StudentServiceTest

---
+++ src/test/java/br/com/squad44/api/StudentServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.squad44.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentServiceTest {
+
+    @Test
+    public void registerTest() {
+
+    }
+}
