a55b3691c1a6df536f846a09923676cf93fced1b
unknown <marcelo96comercial@gmail.com>
2021-03-29 22:52:01 -0300

    Adicionado Getter/Setter e refatorado atributos DonatorDTO

--- src/main/java/br/com/squad44/api/dto/DonatorDTO.java
+++ src/main/java/br/com/squad44/api/dto/DonatorDTO.java
@@ -3,14 +3,10 @@ package br.com.squad44.api.dto;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import br.com.squad44.api.entities.Donation;
 import br.com.squad44.api.entities.Donator;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class DonatorDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,7 +18,7 @@ public class DonatorDTO implements Serializable {
 	private String city;
 	private String state;
 	private String cpf;
-	private List<Donation> donations = new ArrayList<>();
+	private List<Long> donations = new ArrayList<>();
 	
 	public DonatorDTO() {
 		
@@ -36,8 +32,66 @@ public class DonatorDTO implements Serializable {
 		this.city = donator.getCity();
 		this.state = donator.getState();
 		this.cpf = donator.getCpf();
-		this.donations = donator.getDonations();
+		this.donations = donator.getDonations().stream().map(donation -> donation.getId()).collect(Collectors.toList());
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public List<Long> getDonations() {
+		return donations;
+	}
 }
