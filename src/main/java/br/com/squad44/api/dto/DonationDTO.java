6355a200174e381492a4e820cb82f3842c07aa05
unknown <marcelo96comercial@gmail.com>
2021-03-29 22:40:46 -0300

    Adicionado Getter/Setter e refatorado atributos DonationDTO

--- src/main/java/br/com/squad44/api/dto/DonationDTO.java
+++ src/main/java/br/com/squad44/api/dto/DonationDTO.java
@@ -4,13 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import br.com.squad44.api.entities.Donation;
-import br.com.squad44.api.entities.Donator;
-import br.com.squad44.api.entities.ItemOrder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class DonationDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -18,8 +12,8 @@ public class DonationDTO implements Serializable {
 	private Long id;
 	private Integer quantity;
 	private LocalDateTime instant;
-	private Donator donator;
-	private ItemOrder item;
+	private Long donatorId;
+	private Long itemId;
 
 	public DonationDTO() {
 		
@@ -29,7 +23,44 @@ public class DonationDTO implements Serializable {
 		this.id = donation.getId();
 		this.quantity = donation.getQuantity();
 		this.instant = donation.getInstant();
-		this.donator = donation.getDonator();
-		this.item = donation.getItem();
+		this.donatorId = donation.getDonator().getId();
+		this.itemId = donation.getItem().getId();
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public LocalDateTime getInstant() {
+		return instant;
+	}
+
+	public void setDonatorId(Long donatorId) {
+		this.donatorId = donatorId;
+	}
+
+	public Long getDonatorId() {
+		return donatorId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+	
 }
