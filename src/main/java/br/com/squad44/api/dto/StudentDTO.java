6a3521eefca597c026bc87f697a2a350a47e1978
unknown <marcelo96comercial@gmail.com>
2021-03-29 22:57:25 -0300

    Adicionado Getter/Setter e refatorado atributos StudentDTO

--- src/main/java/br/com/squad44/api/dto/StudentDTO.java
+++ src/main/java/br/com/squad44/api/dto/StudentDTO.java
@@ -6,20 +6,18 @@ import br.com.squad44.api.entities.Order;
 import br.com.squad44.api.entities.Parent;
 import br.com.squad44.api.entities.School;
 import br.com.squad44.api.entities.Student;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class StudentDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
-	private School school;
-	private Parent parent;
-	private Order order;
+	private Long schoolId;
+	private String schoolName;
+	private Long parentId;
+	private String parentName;
+	private Long orderId;
 
 	public StudentDTO() {		
 	}
@@ -27,13 +25,67 @@ public class StudentDTO implements Serializable {
 	public StudentDTO (Student student) {
 		this.id = student.getId();
 		this.name = student.getName();
-		this.school = student.getSchool();
-		this.parent = student.getParent();
-		this.order = student.getOrder();
+		this.schoolId = student.getSchool().getId();
+		this.schoolName = student.getSchool().getName();
+		this.parentId = student.getParent().getId();
+		this.parentName = student.getParent().getName();
+		this.orderId = student.getOrder().getId();
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setSchoolId(Long schoolId) {
+		this.schoolId = schoolId;
+	}
+
+	public Long getSchoolId() {
+		return schoolId;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+
+	public Long getParentId() {
+		return parentId;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getOrderId() {
+		return orderId;
 	}
 	
 }
