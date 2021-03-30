0f88b2b113be9cc9c2bb881d95e2622982839876
unknown <marcelo96comercial@gmail.com>
2021-03-30 19:26:53 -0300

    Adicionado metodos para o post-donation

--- src/main/java/br/com/squad44/api/controllers/DonationController.java
+++ src/main/java/br/com/squad44/api/controllers/DonationController.java
@@ -1,15 +1,23 @@
 package br.com.squad44.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.squad44.api.repositories.DonationRepository;
+import br.com.squad44.api.dto.DonationDTO;
+import br.com.squad44.api.services.DonationService;
 
 @RestController
 @RequestMapping("/donation")
 public class DonationController {
-    
+
     @Autowired
-    DonationRepository donationRepository;
+    DonationService service;
+
+    @PostMapping
+    public ResponseEntity<DonationDTO> register(DonationForm form) {
+        return service.register(form);
+    }
 }
--- src/main/java/br/com/squad44/api/services/DonationService.java
+++ src/main/java/br/com/squad44/api/services/DonationService.java
@@ -1,5 +1,21 @@
 package br.com.squad44.api.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import br.com.squad44.api.dto.DonationDTO;
+import br.com.squad44.api.repositories.DonationRepository;
+
 public class DonationService {
+
+    @Autowired
+    DonationRepository repository;
+
+    @Transactional
+    public ResponseEntity<DonationDTO> register(DonationForm form) {
+    
+    }
 
 }
