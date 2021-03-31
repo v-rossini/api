73aca4dc189b2cf1dff26ebbec54ac64b3eb566b
unknown <marcelo96comercial@gmail.com>
2021-03-31 17:38:03 -0300

    Adicionado metodo findByEmail no UserRepository

--- src/main/java/br/com/squad44/api/repositories/UserRepository.java
+++ src/main/java/br/com/squad44/api/repositories/UserRepository.java
@@ -10,5 +10,8 @@ import br.com.squad44.api.entities.User;
 public interface UserRepository extends CrudRepository<User, Long> {
     
     @Query("select u from User u where u.parentId = ?1")
-    Optional<User> getUserByParentId(Long id);
+    Optional<User> findByParentId(Long id);
+
+    @Query("select u from User u where u.email = ?1")
+    Optional<User> findByEmail(String email);
 }
