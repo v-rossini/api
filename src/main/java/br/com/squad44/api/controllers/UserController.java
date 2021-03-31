88b6c0805e0f4b914225f0a3c0791b2914fa80be
unknown <marcelo96comercial@gmail.com>
2021-03-30 22:44:50 -0300

    Adicionado ParentLoginForm

--- src/main/java/br/com/squad44/api/controllers/UserController.java
+++ src/main/java/br/com/squad44/api/controllers/UserController.java
@@ -1,10 +1,23 @@
 package br.com.squad44.api.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.squad44.api.controllers.form.ParentLoginForm;
+
+import br.com.squad44.api.entities.User;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    
+
+    @PostMapping("/parent")
+    public ResponseEntity<User> register(@RequestBody ParentLoginForm form) {
+
+    }
     
 }
