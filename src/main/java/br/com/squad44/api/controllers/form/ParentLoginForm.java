bdc9d70ca89cb0b0800fef6e7aaa6ed242599f63
unknown <marcelo96comercial@gmail.com>
2021-03-31 17:45:46 -0300

    Alterado estrutura do ParentLoginForm

--- src/main/java/br/com/squad44/api/controllers/form/ParentLoginForm.java
+++ src/main/java/br/com/squad44/api/controllers/form/ParentLoginForm.java
@@ -4,6 +4,12 @@ public class ParentLoginForm {
     
     private String email;
     private String password;
+    private String name;
+    private String phone;
+    private String city;
+    private String address;
+    private String state;
+    private String cpf;
 
     public void setEmail(String email) {
         this.email = email;
@@ -19,5 +25,53 @@ public class ParentLoginForm {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    public String getState() {
+        return state;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 }
--- src/main/java/br/com/squad44/api/services/UserService.java
+++ src/main/java/br/com/squad44/api/services/UserService.java
@@ -1,11 +1,14 @@
 package br.com.squad44.api.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.squad44.api.controllers.form.ParentLoginForm;
 import br.com.squad44.api.dto.ParentDTO;
+import br.com.squad44.api.entities.User;
 import br.com.squad44.api.repositories.UserRepository;
 
 @Service
@@ -13,8 +16,16 @@ public class UserService {
 
     @Autowired
     UserRepository repository;
+
+    @Autowired
+    ParentService parentService;
     
     public ResponseEntity<ParentDTO> register(ParentLoginForm form) {
+        Optional<User> user = repository.findByEmail(form.getEmail());
+        if(user.isPresent()) {
 
+        } else {
+
+        }
     }
 }
