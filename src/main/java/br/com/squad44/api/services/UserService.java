26e7762519eead719523e7d95d7e262334b013bf
unknown <marcelo96comercial@gmail.com>
2021-04-01 20:07:08 -0300

    Removido codigo desnecessario no UserService

--- src/main/java/br/com/squad44/api/services/UserService.java
+++ src/main/java/br/com/squad44/api/services/UserService.java
@@ -1,19 +1,11 @@
 package br.com.squad44.api.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import br.com.squad44.api.controllers.form.UserRegisterForm;
-import br.com.squad44.api.controllers.form.UserAuthForm;
-import br.com.squad44.api.dto.DonatorDTO;
-import br.com.squad44.api.dto.ParentDTO;
 import br.com.squad44.api.dto.UserDTO;
-import br.com.squad44.api.entities.Donator;
-import br.com.squad44.api.entities.Parent;
 import br.com.squad44.api.entities.User;
 import br.com.squad44.api.repositories.UserRepository;
 
@@ -33,10 +25,8 @@ public class UserService {
         User user = new User(form.getEmail(), form.getPassword(), form.getCity(), form.getDistrict(), form.getNumber(), 
                             form.getCep(), form.getAddress(), form.getState(), form.getCpf(), form.getName(), form.getPhone());
         repository.save(user);
-        Donator donator = donatorService.register(user).getBody();
-        Parent parent = parentService.register(user).getBody();
-        user.setDonator(donator);
-        user.setParent(parent);
+        user.setDonator(donatorService.register(user).getBody());
+        user.setParent(parentService.register(user).getBody());
         repository.save(user);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
