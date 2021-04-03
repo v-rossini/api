0d3f7e998110719dbb6fae419244de584bbc3cc0
unknown <marcelo96comercial@gmail.com>
2021-04-03 15:53:24 -0300

    Adicionado README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Mochila Cheia
+ API para o projeto do Hackathon FCamara - 2021 Season 1
+
+## Use a Aplicação
+
+Abre o link [https://squad-44-api.herokuapp.com/](https://squad-44-api.herokuapp.com/) no seu browser favorito. Você será direcionado para a página de documentação da API.
+
+![](/.docs/images/swagger-print.PNG)
+
+## Tecnologias utilizadas
+- Java 11
+- Spring Boot
+- Postgresql
+
+## Pré-requisitos
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com), [JDK-11](https://www.oracle.com/br/java/technologies/javase-jdk11-downloads.html), [Maven](https://maven.apache.org/download.cgi).
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+
+## Rodando a Aplicação na sua máquina
+- Clone este repositório através do terminal ``` $ git clone https://github.com/hackathon-squad-44/api ```
+- Acesse a pasta do projeto no terminal ``` $ cd api ```
+- Faça o build e rode a API ``` $ mvn install ```
+- A Aplicação irá subir na seguinte URL:  [http://localhost:8080/](http://localhost:8080/)
--- src/main/java/br/com/squad44/api/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/squad44/api/config/swagger/SwaggerConfiguration.java
@@ -3,20 +3,31 @@ package br.com.squad44.api.config.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
+
+	ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Mochila Cheia API REST")
+            .description("<h4>Spring Boot REST API para o projeto do Hackathon Grupo FCamara - Squad44</h4>")
+            .version("1.0.0")
+            .build();
+    }
     
     @Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("br.com.squad44.api"))
-				.paths(PathSelectors.ant("/**"))
-				.build();				
+				.paths(PathSelectors.ant("/**"))				
+				.build()
+				.apiInfo(apiInfo());
 	}
 }
