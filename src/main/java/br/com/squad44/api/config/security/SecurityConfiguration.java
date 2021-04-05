b60d195ca581f20e7cafe364f69d1c3ed52a618f
unknown <marcelo96comercial@gmail.com>
2021-04-04 21:56:34 -0300

    Adicionado metodo http.cors() no SecurityConfiguration

--- src/main/java/br/com/squad44/api/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/squad44/api/config/security/SecurityConfiguration.java
@@ -15,7 +15,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();        
+        http.csrf().disable();    
+        http.cors();    
     }
 
     @Bean
